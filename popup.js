85c67ff7224041bac6681824d1250675275066e1
stella-sotos <stelsotos@gmail.com>
2016-03-03 15:01:29 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-alert("Hello from your Chrome extension!")
+$(function(){
+  $('#paste').click(function(){pasteSelection();});
+});
+function pasteSelection() {
+  chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
+  function(tab) {
+    chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
+    function(response){
+      var text = document.getElementById('text'); 
+      text.innerHTML = response.data;
+    });
+  });
+}
