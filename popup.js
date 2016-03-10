0d7175cd36a3a7cc9dad3c41597b71296287a3c6
stella-sotos <stelsotos@gmail.com>
2016-03-10 13:00:22 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
 $(function(){
   //changed
-  alert("1");
-  $.getJSON("http://words.bighugelabs.com/api/2/fa90ea6b99f8e8552983850e993a1f7c/cool/json", function(json) {
-         console.log(json);
-         //changed ends
-         alert("2");
+  
   $('#paste').click(function(){pasteSelection();});
 });
 function pasteSelection() {
@@ -12,6 +8,11 @@ function pasteSelection() {
   function(tab) {
     chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
     function(response){
+      alert("1");
+  $.getJSON("http://words.bighugelabs.com/api/2/fa90ea6b99f8e8552983850e993a1f7c/cool/json", function(json) {
+         console.log(json);
+         //changed ends
+         alert("2");
       var text = document.getElementById('text'); 
       text.innerHTML = response.data;
     });
